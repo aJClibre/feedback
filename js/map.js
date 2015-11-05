fcea03fa551148e0ef6942e355e27d27d1a62d36
ajc <ajc@debian8.lan.valabre.com>
2015-11-05 17:06:59 +0100

    A working example of broadcast geolocation
    - modify js/map.js to work with ../real.js

--- js/map.js
+++ js/map.js
@@ -1,3 +1,12 @@
+/*******************
+ * map.js
+ *
+ * npm real.js 
+ * to use a realtime localisation
+ * of all conected users
+ * 
+*******************/
+
 var map = L.map('map', {
 	zoomControl: false });
 
@@ -8,61 +17,44 @@ L.tileLayer('http://{s}.tile.thunderforest.com/outdoors/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
-//var marker = L.marker([43.47312, 5.43947]).addTo(map);
-
 // start the feedback information module
 feed.initModule( 'container', map );
 
 
 /******** realtime location ***********/
 $(function() {
+	var userId, socket, redIcon, yellowIcon, sentData,
+	    markers = [],
+	    sentData = {};
+
 	// generate unique user id
-	var userId = Math.random().toString(16).substring(2,15);
-	var socket = io.connect('http://192.168.111.191:8080');
+	userId = Math.random().toString(16).substring(2,15);
+	socket = io.connect('http://192.168.111.191:8080');
 
-	var info = $('#infobox');
-	var doc = $(document);
-
-	// custom marker's icon styles
-	var tinyIcon = L.Icon.extend({
-		options: {
-			shadowUrl: '../assets/marker-shadow.png',
-			iconSize: [25, 39],
-			iconAnchor:   [12, 36],
-			shadowSize: [41, 41],
-			shadowAnchor: [12, 38],
-			popupAnchor: [0, -30]
-		}
-	});
-	var redIcon = L.AwesomeMarkers.icon({icon: 'file', markerColor: 'blue'});
-	var yellowIcon = L.AwesomeMarkers.icon({icon: 'file', markerColor: 'green'});
-
-	var sentData = {};
-
-	var connects = {};
-	var markers = {};
-	var active = false;
-
-	socket.on('load:coords', function(data) {
-console.dir(data);
-		if (!(data.id in connects)) {
-console.log('socket.on if');
-			setMarker(data);
-		}
-
-		connects[data.id] = data;
-		connects[data.id].updated = $.now();
-	});
+	redIcon = L.AwesomeMarkers.icon({icon: 'file', markerColor: 'blue'});
+	yellowIcon = L.AwesomeMarkers.icon({icon: 'file', markerColor: 'green'});
 
 	// check whether browser supports geolocation api
-	if (navigator.geolocation) {
-		navigator.geolocation.getCurrentPosition(positionSuccess, positionError, { enableHighAccuracy: true });
-	} else {
-		$('.map').text('Your browser is out of fashion, there\'s no geolocation!');
-	}
+        if (navigator.geolocation) {
+                // create the marker and add to the map
+                navigator.geolocation.getCurrentPosition(positionSuccess, positionError, { enableHighAccuracy: true });
+        } else {
+                info.text('Your browser is out of fashion, there\'s no geolocation!');
+        }
 
+	// receive from server side
+	socket.on('load:coords', function(data) {
+//console.dir(data);		
+		var marker;
+		if ( !(data.serverData.id in markers) ) {
+			marker  = L.marker([data.serverData.coords[0].lat, data.serverData.coords[0].lng], { icon: yellowIcon }).addTo(map);
+			marker.bindPopup('<p>external user (' + data.serverData.id  + ')</p>');
+			markers.push(data.serverData.id);
+		}
+	});
+
+	/// if navigator.geolocation === true
 	function positionSuccess(position) {
-console.log('positionSuccess');
 		var lat = position.coords.latitude;
 		var lng = position.coords.longitude;
 		var acr = position.coords.accuracy;
@@ -71,46 +63,19 @@ console.log('positionSuccess');
 		var userMarker = L.marker([lat, lng], {
 			icon: redIcon
 		});
-		// uncomment for static debug
-		// userMarker = L.marker([51.45, 30.050], { icon: redIcon });
 
 		userMarker.addTo(map);
-		userMarker.bindPopup('<p>You are there! Your ID is ' + userId + '</p>').openPopup();
+		userMarker.bindPopup('<p>ID: ' + userId + '</p>').openPopup();
 
-//		var emit = $.now();
-		// send coords on when user is active
-//		doc.on('mousemove', function() {
-			active = true;
-
-			sentData = {
-				id: userId,
-				active: active,
-				coords: [{
-					lat: lat,
-					lng: lng,
-					acr: acr
-				}]
-			};
-console.log('socket.emit()');
-//			if ($.now() - emit > 30) {
-				socket.emit('send:coords', sentData);
-//				emit = $.now();
-//			}
-//		});
-	}
-
-//	doc.bind('mouseup mouseleave', function() {
-//		active = false;
-//	});
-
-	// showing markers for connections
-	function setMarker(data) {
-console.log('setMarker data.id: %s', data.id);
-		for (var i = 0; i < data.coords.length; i++) {
-			var marker = L.marker([data.coords[i].lat, data.coords[i].lng], { icon: yellowIcon }).addTo(map);
-			marker.bindPopup('<p>One more external user is here!</p>');
-			markers[data.id] = marker;
-		}
+		sentData = {
+			id: userId,
+			coords: [{
+				lat: lat,
+				lng: lng,
+				acr: acr
+			}]
+		};
+		socket.emit( 'send:coords', {clientData: sentData} );
 	}
 
 	// handle geolocation api errors
@@ -124,21 +89,9 @@ console.log('setMarker data.id: %s', data.id);
 	}
 
 	function showError(msg) {
-		info.addClass('error').text(msg);
-
-		doc.click(function() {
-			info.removeClass('error');
-		});
+		//info.addClass('error').text(msg);
+		console.log(msg);
 	}
 
-	// delete inactive users every 15 sec
-	setInterval(function() {
-		for (var ident in connects){
-			if ($.now() - connects[ident].updated > 15000) {
-				delete connects[ident];
-				map.removeLayer(markers[ident]);
-			}
-		}
-	}, 50000);
 });
 /*** eo realtime ***/
