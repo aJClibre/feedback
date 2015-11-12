05589e54009be21683d6799c95a4f90568200d7d
ajc <ajc@debian8.lan.valabre.com>
2015-11-12 15:25:20 +0100

    Add watch Locate option to the map inside map.js
    - continuous watching
    - display the ID of other users connected

--- js/map.js
+++ js/map.js
@@ -7,8 +7,10 @@
  * 
 *******************/
 
-var map = L.map('map', {
-	zoomControl: false });
+var 
+	geoloc = false,
+	map = L.map('map', {
+		zoomControl: false });
 
 map.setView([43.4, 5.5], 11);
 
@@ -20,27 +22,60 @@ L.tileLayer('http://{s}.tile.thunderforest.com/outdoors/{z}/{x}/{y}.png', {
 // start the feedback information module
 feed.initModule( 'container', map );
 
+/********* Modernizr activity **********/
+// detect if exists
+if ( Modernizr ) {
+	if ( Modernizr.geolocation ) {
+		geoloc = true;
+	}
+}
+else {
+	console.log('Modernizr NO exists');
+}
+/****** eo Modernizr *******/
 
 /******** realtime location ***********/
 $(function() {
-	var userId, socket, redIcon, yellowIcon, sentData,
-	    markers = [],
-	    sentData = {};
+	var socket, redIcon, yellowIcon, userMarker, userId,
+	    markers 	= {};
 
-	// generate unique user id
-	userId = Math.random().toString(16).substring(2,15);
-	socket = io.connect('http://192.168.111.191:8080');
+	socket  = io.connect('http://192.168.111.191:8080');
 
-	redIcon = L.AwesomeMarkers.icon({icon: 'file', markerColor: 'blue'});
-	yellowIcon = L.AwesomeMarkers.icon({icon: 'file', markerColor: 'green'});
+	redIcon 	= L.AwesomeMarkers.icon({icon: 'file', markerColor: 'blue'});
+	yellowIcon 	= L.AwesomeMarkers.icon({icon: 'file', markerColor: 'green'});
 
 	// check whether browser supports geolocation api
-        if (navigator.geolocation) {
+        if ( geoloc ) {
                 // create the marker and add to the map
-                navigator.geolocation.getCurrentPosition(positionSuccess, positionError, { enableHighAccuracy: true });
+		console.log('geoloc');		
+//                navigator.geolocation.getCurrentPosition(positionSuccess, positionError, { enableHighAccuracy: true, maximumAge: 60000, timeout: 10000 });
         } else {
                 info.text('Your browser is out of fashion, there\'s no geolocation!');
         }
+
+	map.locate({
+        	watch                   : true, // map.stopLocate() to stop
+	        setView                 : false,
+        	timeout                 : 10000, // to wait before firing locationerror
+	        maximumAge              : 10, // Maximum age of detection location
+        	enableHighAccurracy     : true
+	})
+	.on( 'locationfound', positionSuccess ) /*function(e){
+	    console.dir(e);
+            var marker = L.marker([e.latitude, e.longitude]).bindPopup('Your are here :)');
+            var circle = L.circle([e.latitude, e.longitude], e.accuracy/2, {
+                weight: 1,
+                color: 'blue',
+                fillColor: '#cacaca',
+                fillOpacity: 0.2
+            });
+            map.addLayer(marker);
+            map.addLayer(circle);
+        }) */
+       .on('locationerror', function(e){
+            console.log(e);
+            console.log("Location access denied.");
+        });
 
 	// receive from server side
 	socket.on('load:coords', function(data) {
@@ -49,37 +84,49 @@ $(function() {
 		if ( !(data.serverData.id in markers) ) {
 			marker  = L.marker([data.serverData.coords[0].lat, data.serverData.coords[0].lng], { icon: yellowIcon }).addTo(map);
 			marker.bindPopup('<p>external user (' + data.serverData.id  + ')</p>');
-			markers.push(data.serverData.id);
+			markers[data.serverData.id] = marker;
+		}
+		else {
+			markers[data.serverData.id].setLatLng( [data.serverData.coords[0].lat, data.serverData.coords[0].lng] );
 		}
 	});
 
 	/// if navigator.geolocation === true
 	function positionSuccess(position) {
-		var lat = position.coords.latitude;
-		var lng = position.coords.longitude;
-		var acr = position.coords.accuracy;
+		var sentData	= {};
 
-		// mark user's position
-		var userMarker = L.marker([lat, lng], {
-			icon: redIcon
-		});
+		if ( userMarker ) {
+			userMarker.setLatLng( [position.latitude, position.longitude] );
+		}
+		else {
+        		userId  = Math.random().toString(16).substring(2,15); // generate unique user id
+console.dir(position);
+			// mark user's position
+			userMarker = L.marker([position.latitude, position.longitude], {
+				icon: redIcon
+			});
 
-		userMarker.addTo(map);
-		userMarker.bindPopup('<p>ID: ' + userId + '</p>').openPopup();
+			userMarker.addTo(map);
+			userMarker.bindPopup('<p>ID: ' + userId + '</p>').openPopup();
+		}
 
 		sentData = {
-			id: userId,
+			id	: userId,
+			ts      : position.timestamp,
 			coords: [{
-				lat: lat,
-				lng: lng,
-				acr: acr
+				lat	: position.latitude,
+				lng	: position.longitude,
+				north	: position.heading,
+				acr	: position.accuracy
 			}]
 		};
 		socket.emit( 'send:coords', {clientData: sentData} );
 	}
 
 	// handle geolocation api errors
+	// add close socket when location failed
 	function positionError(error) {
+console.dir(error);
 		var errors = {
 			1: 'Authorization fails', // permission denied
 			2: 'Can\'t detect your location', //position unavailable
